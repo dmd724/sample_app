e9cb49ebe996b528b69a5358cd2c2312844d684f
Devin <devin.demici@uconn.edu>
2012-12-09 00:21:55 -0500

    Revert "mess up"
    This reverts commit b85418e87105e2c9d8d856f9a8631435862fc621.

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -33,7 +33,6 @@ describe "User pages" do
       end
 
 <<<<<<< HEAD
-<<<<<<< HEAD
       describe "after saving the user" do
         before { click_button submit }
         
@@ -42,10 +41,6 @@ describe "User pages" do
         it { should have_selector('title', text: user.name) }
         it { should have_selector('div.alert.alert-success', text: 'Welcome') }
         it { should have_link('Sign out') }
-=======
-      it "should create a user" do
-        expect { click_button submit }.to change(User, :count).by(1)
->>>>>>> parent of 3e996d5... Finish sign in
 =======
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,5 +1,4 @@
 <<<<<<< HEAD
-<<<<<<< HEAD
 include ApplicationHelper
 
 def valid_signin(user)
@@ -22,9 +21,6 @@ def sign_in(user)
    # Sign in when not using Capybara as well.
   cookies[:remember_token] = user.remember_token
 end
-=======
-include ApplicationHelper
->>>>>>> parent of 3e996d5... Finish sign in
 =======
 include ApplicationHelper
 >>>>>>> parent of 3e996d5... Finish sign in
